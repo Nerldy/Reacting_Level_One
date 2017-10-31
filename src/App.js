25b69bdd3411191c83da3f9560ac6c57d58216e2
nerldy <arnodhis@gmail.com>
2017-10-31 09:34:09 +0300

    Adding new components for user input and output

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
 import React, {Component} from "react";
 import "./App.css";
 
+// ***Import Components***
+import UserInput from "./UserInput/UserInput";
+import UserOutput from "./UserOutput/UserOutput";
+
+
 class App extends Component {
+    state = {
+        userName: "Default Name"
+    };
+
+    onUserNameClick = event => {
+        this.setState({
+            userName: event.target.value
+        });
+    };
 
     render() {
+        const style = {
+            color: "red"
+        };
         return (
             <div>
-
+                <UserInput changed={this.onUserNameClick}/>
+                <UserOutput userName={"Armin"}/>
+                <UserOutput style={style} userName={this.state.userName}/>
+                <UserOutput changeUserName={this.onUserNameClick}/>
             </div>
         );
     }
