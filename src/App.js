1e1a118c0c0046376e176fed8d344c148f63166e
nerldy <arnodhis@gmail.com>
2017-11-06 16:19:30 +0300

    Doing Assignment

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import React, {Component} from "react";
 import "./App.css";
 
 // ***Import Components***
+import ValidationComponent from "./ValidationComponent/ValidationComponent";
 
-
+import CharComponent from "./CharComponent/CharComponent";
 
 class App extends Component {
 
+    state = {
+        stringLength: ""
+    };
+
+    onCheckLength = e => {
+        this.setState({stringLength: e.target.value});
+    };
+    removerCharacter = () => {
+        this.setState({stringLength: ""});
+    };
+
+    render() {
+        return (
+            <div>
+                <input type="text" onChange={this.onCheckLength} value={this.state.stringLength === 0 ? "" : this.state.stringLength}/>
+                <p>Input Length: {this.state.stringLength.length}</p>
+                <ValidationComponent textLength={this.state.stringLength}/>
+                <CharComponent removeChar={this.removerCharacter} inputText={this.state.stringLength}/>
+            </div>
+        );
+    }
 }
 
 export default App;
